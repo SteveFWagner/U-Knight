aac6db9507726b75a0f20ede755f89564f9833aa
wmichaelbischoff <wmichaelbischoff@gmail.com>
2019-04-05 14:35:59 -0600

    Added Michael.js

---
+++ src/components/Michael.js
@@ -0,0 +1,11 @@
+import react, { Component } from 'react';
+
+class Michael extends Component{
+    render(){
+        return(
+            <div>Hey guyzzzz</div>
+        )
+    }
+}
+
+export default Michael
