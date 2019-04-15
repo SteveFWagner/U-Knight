151ed121bb9d69db2e61545f059512362eb9b713
SteveFWagner <sfwagner1011@gmail.com>
2019-04-15 11:11:11 -0600

    new branch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { withRouter } from 'react-router-dom'
 import Nav from './components/Nav/Nav'
 import routes from './routes'
 import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles'
+import './App.css'
 
 class App extends Component {
 
@@ -13,7 +14,7 @@ class App extends Component {
       }
   })
     return (
-      <div style={{backgroundImage:`url('https://images.unsplash.com/photo-1511512578047-dfb367046420?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80')`, backgroundSize:'100vw 100vh', backgroundAttachment:'fixed', minHeight:'100vh'}}>
+      <div className='app'>
         <MuiThemeProvider theme={theme}>
           <Nav location={this.props.location} history={this.props.history}/>
           {routes}
